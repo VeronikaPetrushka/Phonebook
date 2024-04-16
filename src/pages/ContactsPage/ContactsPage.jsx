1bf6523fe9c74395cc5f121b6bafa75970ca1022
Veronika <veronikapetrushka@gmail.com>
2024-04-16 14:09:08 +0200

    paths  in ContactPage fixed

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { ContactList } from '../components/ContactList/ContactList';
-import { ContactEditor } from '../components/ContactEditor/ContactEditor';
-import { fetchContacts } from '../redux/contacts/operations';
-import { selectLoading } from '../redux/contacts/selectors';
+import ContactList from '../../components/ContactList/ContactList';
+import { ContactEditor } from '../../components/ContactEditor/ContactEditor';
+import { fetchContacts } from '../../redux/contacts/operations';
+import { selectLoading } from '../../redux/contacts/selectors';
 
 export default function ContactsPage() {
   const dispatch = useDispatch();
