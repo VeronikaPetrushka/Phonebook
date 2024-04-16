77dae0ffb836c9bdfb22449ef6478e006f0bb1bc
Veronika <veronikapetrushka@gmail.com>
2024-04-16 15:25:52 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,3 @@
-// import './App.css';
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import ContactForm from './components/ContactFrom/ContactForm';
-// import SearchBox from './components/SearchBox/SearchBox';
-// import ContactList from './components/ContactList/ContactList';
-// import { fetchContacts } from './redux/contacts/operations';
-
-// const App = () => {
-
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(fetchContacts());
-//   }, [dispatch]);
-
-//   return (
-//     <div>
-//       <h1 className='PhoneBookTittle'>Welcome to your phonebook</h1>
-//       <ContactForm />
-//       <SearchBox />
-//       <ContactList />
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import { useEffect, lazy } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
